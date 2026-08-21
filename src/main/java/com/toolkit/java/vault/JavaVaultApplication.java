ba1fad26f03e9d64fcac00d155e75b004e67db88
Manish Poojary <manishh.poojary02@gmail.com>
2026-08-22 00:12:45 +0530

    repo setup

---
+++ src/main/java/com/toolkit/java/vault/JavaVaultApplication.java
@@ -0,0 +1,13 @@
+package com.toolkit.java.vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaVaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toolkit/java/vault/JavaVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolkit.java.vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
